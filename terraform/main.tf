c6c5954bc169b0b87839d72bb8308ae0a84b2dae
mirr254 <sammysteppa90@gmail.com>
2020-09-21 14:06:52 +0300

    fix missing route table

--- terraform/main.tf
+++ terraform/main.tf
@@ -49,19 +49,72 @@ resource "aws_route_table" "public_route_table" {
   }
 }
 
-resource "aws_route_table_associatiation" {
+resource "aws_route_table_associatiation" "public-route-1-association" {
   route_table_id = "${aws_route_table.public_route_table.id}"
   subnet_id      = "${aws_subnet.public_subnet_1.id}"
 }
 
-resource "aws_route_table_associatiation" {
+resource "aws_route_table_associatiation" "public-route-2-association" {
   route_table_id = "${aws_route_table.public_route_table.id}"
   subnet_id      = "${aws_subnet.public_subnet_2.id}"
 }
 
-resource "aws_route_table_associatiation" {
+resource "aws_route_table_associatiation" "public-route-3-association" {
   route_table_id = "${aws_route_table.public_route_table.id}"
   subnet_id      = "${aws_subnet.public_subnet_3.id}"
+}
+
+resource "aws_subnet" "private-subnet-1" {
+  cidr_block        = "${var.private_subnet_1_cidr}"
+  vpc_id            = "${aws_vpc.production-vpc.id}"
+  availability_zone = "us-east-2a"
+
+  tags {
+    Name = "private-subnet-1"
+  }
+}
+
+resource "aws_subnet" "private-subnet-2" {
+  cidr_block        = "${var.private_subnet_2_cidr}"
+  vpc_id            = "${aws_vpc.production-vpc.id}"
+  availability_zone = "us-east-2b"
+
+  tags {
+    Name = "private-subnet-2"
+  }
+}
+
+resource "aws_subnet" "private-subnet-3" {
+  cidr_block        = "${var.private_subnet_3_cidr}"
+  vpc_id            = "${aws_vpc.production-vpc.id}"
+  availability_zone = "us-east-2c"
+
+  tags {
+    Name = "private-subnet-3"
+  }
+}
+
+resource "aws_route_table" "private-route-table" {
+  vpc_id        = "${aws_vpc.production-vpc.id}"
+
+  tags {
+    Name = "Private-Route-Table"
+  }
+}
+
+resource "aws_route_table_association" "private-route-1-association" {
+  route_table_id = "${aws_route_table.private-route-table.id}"
+  subnet_id      = "${aws_subnet.private-subnet-1.id}"
+}
+
+resource "aws_route_table_association" "private-route-2-association" {
+  route_table_id = "${aws_route_table.private-route-table.id}"
+  subnet_id      = "${aws_subnet.private-subnet-2.id}"
+}
+
+resource "aws_route_table_association" "private-route-3-association" {
+  route_table_id = "${aws_route_table.private-route-table.id}"
+  subnet_id      = "${aws_subnet.private-subnet-3.id}"
 }
 
 resource "aws_eip" "elastic-ip-for-nat-gtw" {
@@ -99,7 +152,7 @@ resource "aws_internet_gateway" "production-internet-gtw" {
 }
 
 resource "aws_route" "public-internet-gtw-route" {
-  route_table_id   = "${aws_route_table.public-route-table}"
+  route_table_id   = "${aws_route_table.public-route-table.id}"
   gateway_id       = "${aws_internet_gateway.production-internet-gtw.id}"
   destination_cidr_block = "0.0.0.0/0"
 }
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -17,3 +17,15 @@ variable "public_subnet_2_cidr" {
 variable "public_subnet_3_cidr" {
   description = "CIDR block for public subnent 3"
 }
+
+variable "private_subnet_1_cidr" {
+  description = "CIDR Block for Private Subnet 1"
+}
+
+variable "private_subnet_2_cidr" {
+  description = "CIDR Block for Private Subnet 2"
+}
+
+variable "private_subnet_3_cidr" {
+  description = "CIDR Block for Private Subnet 3"
+}
